91d588782c297ae4cd062163ee793550d113e6af
Versifiction <marc_charpentier@hotmail.fr>
2019-12-29 23:50:49 +0100

    fonctionnel

--- server/index.js
+++ server/index.js
@@ -19,11 +19,11 @@ require("dotenv").config();
 
 app.use(morgan("tiny"));
 
-app.use(express.static(path.join(__dirname, "../../client/build")));
+// app.use(express.static(path.join(__dirname, "/../client/build")));
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname + "../../../client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname + "/../client/build/index.html"));
+// });
 
 // var whitelist = [
 //   process.env.CLIENT_PORT,
