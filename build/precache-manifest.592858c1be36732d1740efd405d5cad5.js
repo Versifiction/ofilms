73f83444ecbe4a54cc941e5c3eb027a1a14f2a8e
Marc Charpentier <MARC_C@scpp.fr>
2019-12-30 16:30:35 +0100

    test avec proxy package json

---
+++ build/precache-manifest.592858c1be36732d1740efd405d5cad5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "756681faaff636d2aef3cdb224623603",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c35148f953d379024161",
+    "url": "/static/css/main.b712e0da.chunk.css"
+  },
+  {
+    "revision": "e66c879e24bf18f5a9d1",
+    "url": "/static/js/2.1bcae49f.chunk.js"
+  },
+  {
+    "revision": "c35148f953d379024161",
+    "url": "/static/js/main.22615257.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e5ebe40353acd8b5b15230c40436efc4",
+    "url": "/static/media/404.e5ebe403.jpg"
+  },
+  {
+    "revision": "cb3736c81b31c988b8b6f9851cb93939",
+    "url": "/static/media/bg-films-edit.cb3736c8.jpg"
+  },
+  {
+    "revision": "c242110c5aea83e0b27e0ecff8250906",
+    "url": "/static/media/bg-header.c242110c.jpg"
+  },
+  {
+    "revision": "d66d3c9baca3baa2af3988aa4e96d386",
+    "url": "/static/media/bg-series-edit.d66d3c9b.jpg"
+  },
+  {
+    "revision": "cb376e6045de5df9dd30dfec08d1cf9a",
+    "url": "/static/media/movies-parallax.cb376e60.jpg"
+  }
+]);
