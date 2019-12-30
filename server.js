7b5eecc950b00a8d4d59dcfe8744030d470fafbd
Versifiction <marc_charpentier@hotmail.fr>
2019-12-30 18:34:24 +0100

    retour au proxy dans le package json

--- server.js
+++ server.js
@@ -19,13 +19,13 @@ require("dotenv").config();
 
 app.use(morgan("tiny"));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/build/index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname + "/build/index.html"));
+//   });
+// }
 
 // var whitelist = [
 //   process.env.CLIENT_PORT,
